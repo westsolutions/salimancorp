353685bd41399db9764136c0ec9d0173da8551fd
Kote Stroi <catia.stroi@gmail.com>
2019-03-12 12:43:21 +0200

    styled page V2

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,26 @@
 $(function(){
   console.log('app')
-  $('.your-class').slick({
+  $('.hero-slider').slick({
   	arrows: false,
-  	dots: true
+  	dots: true,
+  	vertical: true,
+  	adaptiveHeight: true,
+  	autoplay: true,
+  	speed: 1000,
+    autoplaySpeed: 2000
   });
+});
+
+$('.navbar-nav').on('click', ' a[href^="#"]', function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top}, 1000);
+});
+
+
+$(document).ready(() => {
+    $('.nav-item').click(() => {
+    	$( ".navbar-collapse" ).removeClass( "show" );
+        $('body').toggleClass('disabled-scrolling');
+    });
 });
