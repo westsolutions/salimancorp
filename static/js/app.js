e649abba67e75998726bd959e46730a3ed93649d
taras <tk@wwwest.solutions>
2019-03-12 23:12:16 +0200

    ui improvements

--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,57 @@
-$(function(){
-  console.log('app')
-  $('.hero-slider').slick({
-  	arrows: false,
-  	dots: true,
-  	vertical: true,
-  	slidesToShow: 1,
-  	slidesToScroll:1,
-  	//adaptiveHeight: true,
-  	appendDots: $(".hero-slider-dots"),
-  	/*autoplay: true,
-  	speed: 1000,
-    autoplaySpeed: 2000*/
-  });
-});
+(function () {
+	$(document).ready(function () {
+		$('.nav-item').click(function () {
+			$(".navbar-collapse").removeClass("show");
+			$('body').toggleClass('disabled-scrolling');
+		});
 
-$('.navbar-nav').on('click', ' a[href^="#"]', function (event) {
-    event.preventDefault();
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top}, 1000);
-});
+		$('.navbar-nav').on('click', 'a[href^="#"]', function (event) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: $($.attr(this, 'href')).offset().top
+			}, 1000);
+		});
 
+		$('.hero-slider').slick({
+			arrows: false,
+			dots: true,
+			vertical: true,
+			adaptiveHeight: true,
+			// slidesToShow: 1,
+			// slidesToScroll: 1,
+			// adaptiveHeight: true,
+			appendDots: $(".hero-slider-dots"),
+			centerMode: true,
+			centerPadding: 100
+			/*autoplay: true,
+			speed: 1000,
+			autoplaySpeed: 2000*/
+		});
 
-$(document).ready(() => {
-    $('.nav-item').click(() => {
-    	$( ".navbar-collapse" ).removeClass( "show" );
-        $('body').toggleClass('disabled-scrolling');
-    });
-});
+		$('.js-contact-form').submit(function (ev) {
+			ev.preventDefault();
+			var data = $(this).serialize();
+			
+			$.ajax({
+				method: 'POST',
+				url: window.location.origin + window.location.pathname + 'mailer.php',
+				data: data,
+				dataType: 'json',
+				encode: true,
+			}).always(function (data) {
+				if (data.success) {
+					var name = $('.js-name-input').val();
+					$('js-name').text('');
+					$('.js-send-success').modal('show');
+					$('.js-contact-form input').each(function () {
+						$(this).val('');
+					});
+					$('.js-contact-form textarea').html('')
+				}
+				else {
+					console.log(XMLHttpRequest, textStatus, errorThrown);
+				}
+			});
+		});
+	});
+})(jQuery);
