297c2fd56d150ce66f53df29261826fe84e501f0
taras <tk@wwwest.solutions>
2019-03-13 00:04:31 +0200

    fix mailgun

--- mailer.php
+++ mailer.php
@@ -6,21 +6,19 @@ use Mailgun\Mailgun;
 $domain = 'sandbox44b8a087b0004b179900331042bf92a5.mailgun.org';
 $API_KEY = '0299ec27ce0798d3d729ecf61f27a1e2-de7062c6-654fc694';
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 if (isset($_POST['name']) && $_POST['email'] && $_POST['message']) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+    $name = ucfirst($_POST['name']);
+    $email = strtolower($_POST['email']);
+    $message = ucfirst($_POST['message']);
     $mg = Mailgun::create($API_KEY);
+    $args = array(
+        'from'    => 'Mr Soliman <info@'.$domain.'>',
+        'to'      => $name. ' <'.$email.'>',
+        'subject' => 'Hi, '. $name,
+        'text'    => 'Thanks for you message. I\'ll contact with you soon!'
+    );
 
-    $result = $mg->messages()->send($domain, [
-    'from'    => 'Mr Soliman <info@'.$domain.'>',
-    'to'      => $name. ' <'.$email.'>',
-    'subject' => 'Hi, '. $name,
-    'text'    => 'Thanks for you message. I\'ll contact with you soon!'
-    ]);
+    $result = $mg->messages()->send($domain, $args);
 
     if ($result) {
         echo json_encode(array('success' => true));
